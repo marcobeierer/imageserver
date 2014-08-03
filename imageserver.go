8e70299290d4d453fd6630c191cd54f0432c7ae3
marco <marco@webguerilla.net>
2014-08-04 00:47:22 +0200

    added configuration flags implemented protection against some sort of dos attack

--- imageserver.go
+++ imageserver.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/nfnt/resize"
 	"image/jpeg"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -15,10 +17,12 @@ func main() {
 
 	imageserver := new(ImageServer)
 
-	imageserver.Port = 9999
-	imageserver.ImagesPath = "images"
-	imageserver.ImageFileTypes = []string{"jpg", "jpeg"}
+	imageserver.Port = *flag.Int("port", 9999, "the port number of the imageserver")
+	imageserver.ImagesPath = *flag.String("imagespath", "images", "path to the images directory")
 
+	flag.Parse()
+
+	imageserver.ImageFileTypes = []string{"jpg", "jpeg"} // TODO add support for png and gif
 	imageserver.run()
 }
 
@@ -62,13 +66,19 @@ func (is *ImageServer) ImageHandler(writer http.ResponseWriter, request *http.Re
 
 	width, err := getQueryIntValue(query, "width")
 	if err != nil {
+
 		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(writer, "invalid width value")
+
 		return
 	}
 
 	height, err := getQueryIntValue(query, "height")
 	if err != nil {
+
 		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(writer, "invalid height value")
+
 		return
 	}
 
@@ -82,15 +92,54 @@ func (is *ImageServer) ImageHandler(writer http.ResponseWriter, request *http.Re
 
 	file, err := os.Open(imagePath)
 	if err != nil {
+
 		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "file not found")
+
 		return
 	}
+	defer file.Close()
 
 	image, err := jpeg.Decode(file)
 	if err != nil {
+
+		log.Printf("decoding image failed: %s\n", err.Error())
+
 		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(writer, "internal server error")
+
+		return
 	}
-	file.Close()
+
+	_, err = file.Seek(0, 0) // necessary for reading config
+	if err != nil {
+
+		log.Printf("seek failed: %s\n", err.Error())
+
+		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(writer, "internal server error")
+
+		return
+	}
+
+	config, err := jpeg.DecodeConfig(file) // TODO could also use image instead of jpeg package
+	if err != nil {
+
+		log.Printf("decoding config failed: %s\n", err.Error())
+
+		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(writer, "internal server error")
+
+		return
+	}
+
+	if width > uint(config.Width) || height > uint(config.Height) {
+
+		writer.WriteHeader(http.StatusBadRequest) // TODO return more detailed error message or image at original size?
+		fmt.Fprint(writer, "width and/or height value is greater than the original image dimensions")
+
+		return
+	}
 
 	resizedImage := resize.Resize(width, height, image, resize.Lanczos3)
 	jpeg.Encode(writer, resizedImage, nil)
